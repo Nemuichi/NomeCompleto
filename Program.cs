d0e9609481c78e5c790985fdca3991fc311d7207
Ramon Dias <ramondiasperes04@gmail.com>
2025-09-19 22:40:46 -0300

    Atividade Concluída

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+string nome, sobrenome, nomeCompleto;
+
+nome = "Ramon";
+sobrenome = " Peres";
+nomeCompleto = nome + sobrenome;
+
+Console.WriteLine($"Digite seu primeiro nome: {nome}");
+
+Console.WriteLine($"Digite seu sobrenome:{sobrenome}");
+
+Console.WriteLine($"Nome completo: {nomeCompleto}");
+
+
+Console.Write($"Nome de catálogo:{sobrenome.ToUpper()}, {nome}");
